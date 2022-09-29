09116eae6efe8fcdfe271e933f12498074440abe
Ethan <ethangely@free.fr>
2022-09-29 13:54:41 +0200

    à voir...

--- Controller/Index.js
+++ Controller/Index.js
@@ -5,7 +5,7 @@
 // import {Plat} from '../class/Plat';
 // import {Menu} from '../class/Menu';
 // import {Reservation} from '../class/Reservation';
-require(['../Model/Restaurant']);
+require(['../Model/Restaurant'], Restaurant);
 require(['../Model/Employe']);
 require(['../Model/Salaires']);
 require(['../Model/Stock']);
@@ -15,7 +15,7 @@ require(['../Model/Reservation']);
 const VoyageCulinaire = new Restaurant("Voyage culinaire","12, rue de la vierge","06 12 12 12 12",TypeRestaurant.Bistro,"12h-14h & 19h-23h","30");
 
 //employés
-const Sarah = new Employe("Sarah","Gely","35, route du Coquelicot","06 56 85 94 16","FR6210096000502376951846D60",Categorie.Employe_cuisine);
+const Sarah = new Employe("Sarah","Killic","35, route du Coquelicot","06 56 85 94 16","FR6210096000502376951846D60",Categorie.Employe_cuisine);
 const Quaivine = new Employe("Quaivine","Michard","106, rue du potier","06 57 85 64 16","FR4312739000504738673495Y48",Categorie.Employe_cuisine);
 const Pizza = new Employe("Pizza","Steve","2, rue de chez moi","06 57 85 64 16","FR7614508000407221865687I73",Categorie.Serveur);
 const Catherine = new Employe("Catherine","Bonnevue","79, avenue de la S.P.A","07 95 10 06 46","FR7230003000403484712973K89",Categorie.Administratif);
